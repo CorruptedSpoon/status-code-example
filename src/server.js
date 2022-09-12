5b800e03cce80e9bdc0ec96a8f242a44d805b4bc
Van Edelman (RIT Student) <vae3742@rit.edu>
2022-09-12 13:51:16 -0400

    in class

--- src/server.js
+++ src/server.js
@@ -14,7 +14,16 @@ const urlStruct = {
 };
 
 const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
+  console.log(request.url);
+  console.log(parsedUrl);
 
+  if(urlStruct[parsedUrl.pathname]){
+    urlStruct[parsedUrl.pathname](request, response, params);
+  } else {
+    urlStruct.notFound(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
